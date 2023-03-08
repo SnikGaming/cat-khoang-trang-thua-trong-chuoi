a76dae7d65c39cb16d674d687e68e2ab8178b258
TranThoiLong <92561556+SnikGaming@users.noreply.github.com>
2023-03-09 01:20:08 +0700

    Update cat_chuoi.dart

--- cat_chuoi.dart
+++ cat_chuoi.dart
@@ -4,3 +4,8 @@ void main() {
   String result = words.join(" ");
   print(result);
 }
+
+// Phương thức `trim()` được sử dụng để loại bỏ các ký tự khoảng trắng dư thừa ở đầu và cuối chuỗi.
+// Phương thức `split()` được sử dụng để tách từng từ trong chuỗi thành một mảng các chuỗi, với ký tự " " làm dấu hiệu tách các từ.
+// Hàm `where((s) => s.isNotEmpty)` được sử dụng để lọc và loại bỏ các phần tử trống trong mảng kết quả.
+// Phương thức `join()` được sử dụng để ghép các từ trong mảng thành một chuỗi mới, với ký tự " " làm ký tự phân cách giữa các từ.
